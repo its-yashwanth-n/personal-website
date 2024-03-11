09a9bb3157e6f6e454445e9c1b02dea0705c962e
Yashwanth <its.yashwanth.n@gmail.com>
2024-03-11 22:24:58 +1100

    feat: updated config and package

--- next.config.js
+++ next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+    reactStrictMode: true,
+    swcMinify: true,
+    modularizeImports: {
+        "react-icons/fi": {
+            transform: "react-icons/fi/{{member}}",
+        },
+        "react-icons/ai": {
+            transform: "react-icons/ai/{{member}}",
+        },
+        "react-icons/bs": {
+            transform: "react-icons/bs/{{member}}",
+        },
+    },
+    webpack: config => {
+        config.module.rules.push({
+            test: /(Light_Logo|Dark_Logo)\.svg$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'images/[hash]-[name].[ext]',
+                        publicPath: `/_next/static/images/`,
+                        outputPath: 'static/images',
+                    },
+                },
+            ],
+        });
+
+        return config;
+    }
+})
+
